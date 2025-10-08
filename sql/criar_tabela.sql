9caba46beaf778b0b022b489667004b00d9744c7
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-10-08 02:49:57 -0300

    atualizando cria e apaga .sql

--- sql/criar_tabela.sql
+++ sql/criar_tabela.sql
@@ -1,9 +1,11 @@
 CREATE TABLE vendas (
-    id number generated always as identity,
-    nome_comprador varchar2(20),
-    nome_vendedor varchar2(30),
-    data_venda date,
-    descricao_produtos varchar2(150),
-    valor_total number,
-    valor_impostos number
-)
+  id                  NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  nome_comprador      VARCHAR2(60)  NOT NULL,
+  nome_vendedor       VARCHAR2(60)  NOT NULL,
+  data_venda          DATE          NOT NULL,
+  descricao_produtos  VARCHAR2(200) NOT NULL,
+  valor_total         NUMBER(10,2)  NOT NULL,
+  valor_impostos      NUMBER(10,2)  NOT NULL,
+  CONSTRAINT vendas_ck_valores
+    CHECK (valor_total >= 0 AND valor_impostos >= 0 AND valor_impostos <= valor_total)
+);
